6bd24f28399bb94cb89c2336e59e07023d0b93c0
okaz <dish.ocean@gmail.com>
2014-04-27 18:04:34 +0900

    second commit

---
+++ A10-01.c
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+
+int main(){
+	static int x = 0;
+
+	printf("静的記憶域期間が与えられた\nオブジェクトxのアドレスは%dです。\n",&x);
+
+	return 0;
+}
---
+++ A10-02.c
@@ -0,0 +1,25 @@
+﻿#include<stdio.h>
+
+int main(){
+	int a=45,b=876;
+	int *p,*r;
+	int temp;
+
+	printf("変数a: %d\n",a);
+	printf("変数b: %d\n",b);
+
+	printf("ポインタを用いて上記の変数の値を交換します。\n");
+
+	p = &a; /* 変数aのポインタを*pに記憶 */
+	r = &b; /* 変数bのポインタを*rに記憶 */
+
+	/* ポインタ変数p,rを利用して変数aと変数bの値を交換する */
+	temp = a;
+	*p = b;
+	*r = temp;
+
+	printf("変数a: %d\n",a);
+	printf("変数b: %d\n",b);
+
+	return 0;
+}
