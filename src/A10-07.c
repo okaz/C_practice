cf6b819082517d7e601a4e5d5cdde51cec0f1089
okaz <dish.ocean@gmail.com>
2014-04-30 02:32:19 +0900

    Add Source Files

---
+++ src/A10-07.c
@@ -0,0 +1,24 @@
+﻿#include<stdio.h>
+
+int main(){
+	int *ptr;
+	int vc[5]={11,22,33,44,55};
+	int i;
+
+	ptr = &vc[0];
+
+	printf("配列の要素の値を表示します。\n\n");
+
+	printf("====添字演算子 [] を適応した場合====\n");
+	for(i=0;i<5;i++){
+		printf("%d番目の数値: %d\n",i+1, vc[i]);
+	}
+
+
+	printf("====間接演算子 * を適応した場合=====\n");
+	for(i=0;i<5;i++){
+		printf("%d番目の数値: %d\n",i+1, *(ptr+i));
+	}
+
+	return 0;
+}
